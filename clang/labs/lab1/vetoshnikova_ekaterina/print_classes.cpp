528cacb3739bd7809dc1bb24823995613fe64398
Veto1234-ii <vetoshnikova14@bk.ru>
2024-03-24 23:06:29 +0300

    clangformat

--- clang/labs/lab1/vetoshnikova_ekaterina/print_classes.cpp
+++ clang/labs/lab1/vetoshnikova_ekaterina/print_classes.cpp
@@ -24,10 +24,6 @@ public:
   explicit Consumer(clang::ASTContext *Context) : vis(Context) {}
 
   virtual void HandleTranslationUnit(clang::ASTContext &Context) {
-
-    // Traversing the translation unit decl via a RecursiveASTVisitor
-    // will visit all nodes in the AST.
-
     vis.TraverseDecl(Context.getTranslationUnitDecl());
   }
 
@@ -37,25 +33,19 @@ private:
 
 class Plugin : public clang::PluginASTAction {
 public:
-    virtual std::unique_ptr<clang::ASTConsumer>
-    CreateASTConsumer(clang::CompilerInstance &Compiler, 
-        llvm::StringRef InFile) {
+  virtual std::unique_ptr<clang::ASTConsumer>
+  CreateASTConsumer(clang::CompilerInstance &Compiler, llvm::StringRef InFile) {
     return std::unique_ptr<clang::ASTConsumer>(
         new Consumer(&Compiler.getASTContext()));
   }
-
-  //Create the AST consumer object for this action, if supported.
-  // Parameters
-  //CI - The current compiler instance
-  //InFile - The current input file
-  //Returns The new AST consumer, or null on failure.
-
+  
   bool ParseArgs(const clang::CompilerInstance &CI,
                  const std::vector<std::string> &args) {
     if (args.size() && args[0] == "help")
       PrintHelp(llvm::errs());
     return true;
-    }
+
+  }
   void PrintHelp(llvm::raw_ostream &ros) {
     ros << "This plugin prints the names of all classes and their fields\n";
   }

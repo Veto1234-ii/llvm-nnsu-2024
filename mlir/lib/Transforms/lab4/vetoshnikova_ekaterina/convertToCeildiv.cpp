9219c9fa66c423c160e33abf3bf939df40bd6ea8
Veto1234-ii <vetoshnikova14@bk.ru>
2024-05-29 21:03:36 +0300

    fix

---
+++ mlir/lib/Transforms/lab4/vetoshnikova_ekaterina/convertToCeildiv.cpp
@@ -0,0 +1,70 @@
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Tools/Plugins/PassPlugin.h"
+
+using namespace mlir;
+
+namespace {
+
+class VetoshnikovaConvertPass
+    : public PassWrapper<VetoshnikovaConvertPass,
+                         OperationPass<LLVM::LLVMFuncOp>> {
+
+public:
+  StringRef getArgument() const final { return "vetoshnikova-convert-pass"; }
+  StringRef getDescription() const final {
+    return "pass that breaks arith.ceildivui and arith.ceildivsi operations "
+           "into calculation following the rule: ceildiv(a, b) = (a + b - 1) / "
+           "b ";
+  }
+
+  void runOnOperation() override {
+    getOperation()->walk([&](Operation *op) {
+      if (auto ceilDivUI = dyn_cast<arith::CeilDivUIOp>(op)) {
+        replaceCeildiv(ceilDivUI, false);
+      } else if (auto ceilDivSI = dyn_cast<arith::CeilDivSIOp>(op)) {
+        replaceCeildiv(ceilDivSI, true);
+      }
+    });
+  }
+
+private:
+  void replaceCeildiv(Operation *op, bool isSigned) {
+
+    OpBuilder builder(op);
+    Location loc = op->getLoc();
+    Value a = op->getOperand(0);
+    Value b = op->getOperand(1);
+
+    Value one = builder.create<arith::ConstantIntOp>(
+        loc, 1, builder.getIntegerType(32));
+    Value add = builder.create<arith::AddIOp>(loc, a, b);
+    Value sub = builder.create<arith::SubIOp>(loc, add, one);
+    Value div;
+
+    if (isSigned) {
+      div = builder.create<arith::DivSIOp>(loc, sub, b);
+    } else {
+      div = builder.create<arith::DivUIOp>(loc, sub, b);
+    }
+
+    op->replaceAllUsesWith(ValueRange(div));
+    op->erase();
+  }
+};
+} // anonymous namespace
+
+MLIR_DECLARE_EXPLICIT_TYPE_ID(VetoshnikovaConvertPass)
+MLIR_DEFINE_EXPLICIT_TYPE_ID(VetoshnikovaConvertPass)
+
+PassPluginLibraryInfo getPluginInfo() {
+  return {MLIR_PLUGIN_API_VERSION, "vetoshnikova-convert-pass",
+          LLVM_VERSION_STRING,
+          []() { PassRegistration<VetoshnikovaConvertPass>(); }};
+}
+
+extern "C" LLVM_ATTRIBUTE_WEAK PassPluginLibraryInfo mlirGetPassPluginInfo() {
+  return getPluginInfo();
+}
---
+++ mlir/test/Transforms/lab4/vetoshnikova_ekaterina/test.mlir
@@ -0,0 +1,24 @@
+// RUN: mlir-opt -load-pass-plugin=%mlir_lib_dir/VetoshnikovaConvertPlugin%shlibext --pass-pipeline="builtin.module(llvm.func(vetoshnikova-convert-pass))" %s | FileCheck %s
+
+module {
+llvm.func @testceildivui(%arg0: i32, %arg1: i32) -> i32 attributes {llvm.linkage = #llvm.linkage<external>} {
+    // CHECK: llvm.func @testceildivui(%arg0: i32, %arg1: i32) -> i32 attributes {llvm.linkage = #llvm.linkage<external>} {
+    // CHECK-NEXT: %c1_i32 = arith.constant 1 : i32
+    // CHECK-NEXT: %0 = arith.addi %arg0, %arg1 : i32
+    // CHECK-NEXT: %1 = arith.subi %0, %c1_i32 : i32
+    // CHECK-NEXT: %2 = arith.divui %1, %arg1 : i32
+    // CHECK-NOT: %0 = arith.ceildivui %arg0, %arg1 : i32
+    %0 = arith.ceildivui %arg0, %arg1 : i32
+    llvm.return %0 : i32
+}
+llvm.func @testceildivsi(%arg0: i32, %arg1: i32) -> i32 attributes {llvm.linkage = #llvm.linkage<external>} {
+    // CHECK: llvm.func @testceildivsi(%arg0: i32, %arg1: i32) -> i32 attributes {llvm.linkage = #llvm.linkage<external>} {
+    // CHECK-NEXT: %c1_i32 = arith.constant 1 : i32
+    // CHECK-NEXT: %0 = arith.addi %arg0, %arg1 : i32
+    // CHECK-NEXT: %1 = arith.subi %0, %c1_i32 : i32
+    // CHECK-NEXT: %2 = arith.divsi %1, %arg1 : i32
+    // CHECK-NOT: %0 = arith.ceildivsi %arg0, %arg1 : i32
+    %0 = arith.ceildivsi %arg0, %arg1 : i32
+    llvm.return %0 : i32
+}
+}
